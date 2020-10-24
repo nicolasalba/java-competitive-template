7dfeef98fedd3c8149e89061179adcc57f19fed6
Nicolas Alba <nicolas.alba@fundacion-jala.org>
2020-10-24 18:19:51 -0400

    Map implementation

--- src/main/java/hopscotch.java
+++ src/main/java/hopscotch.java
@@ -1,5 +1,5 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class hopscotch {
     public static void main(String[] args) throws IOException {
@@ -9,22 +9,30 @@ public class hopscotch {
 
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        int[][] board = new int[n][n];
+
+        Map<Integer, List<Position>> board = new HashMap<>();
+        for (int i = 1; i <= k; i++) {
+            board.put(i, new ArrayList<>());
+        }
+
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
+                Position position = new Position(i, j);
+                int value = Integer.parseInt(st.nextToken());
+                board.get(value).add(position);
             }
         }
 
         final Solver solver = new Solver();
-        bw.write(String.valueOf(solver.solve(n, k, board, 0, -1, -1, 0)));
+        Position startPosition = new Position(-1, -1);
+        bw.write(String.valueOf(solver.solve(n, k, board, 0, startPosition, 0)));
         bw.flush();
     }
 }
 
 class Solver {
-    public int solve(int n, int k, int[][] board, int currentK, int ai, int aj, int previousResult) {
+    public int solve(int n, int k, Map<Integer, List<Position>> board, int currentK, Position beforePosition, int previousResult) {
         if (previousResult == -1) {
             return -1;
         }
@@ -32,26 +40,46 @@ class Solver {
             return 0;
         }
         int minPath = Integer.MAX_VALUE;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == currentK + 1) {
-                    int totalDistance =  getDistance(aj, j, ai, i) + solve(n, k, board, currentK + 1, i, j, previousResult);
-                    if (totalDistance < minPath) {
-                        minPath = totalDistance;
-                    } else if (totalDistance == 0) {
-                        return -1;
-                    }
-                }
+        for (Position position : board.get(currentK + 1)) {
+            int totalDistance =  getDistance(beforePosition, position) + solve(n, k, board, currentK + 1, position, previousResult);
+            if (totalDistance < minPath) {
+                minPath = totalDistance;
+            } else if (totalDistance == 0) {
+                return -1;
             }
         }
         return minPath == Integer.MAX_VALUE ? -1 : minPath + previousResult;
     }
 
-    private int getDistance(int x1, int x2, int y1, int y2) {
-        if (x1 == -1) {
+    private int getDistance(Position beforePosition, Position currentPosition) {
+        if (beforePosition.x == -1) {
             return 0;
         }
-        return Math.abs(x2 - x1) + Math.abs(y2 - y1);
+        return Math.abs(beforePosition.x - currentPosition.x) + Math.abs(beforePosition.y - currentPosition.y);
     }
 
+}
+
+class Position {
+    int x;
+    int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return x == position.x &&
+                y == position.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
 }
