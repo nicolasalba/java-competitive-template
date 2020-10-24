88fd0b17427b07b1ebe062d457de2518b6fd08e5
Nicolas Alba <nicolas.alba@fundacion-jala.org>
2020-10-24 09:52:57 -0400

    IO was added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        final StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long n = Long.parseLong(st.nextToken());
+
+        final Solver solver = new Solver();
+        bw.write(String.valueOf(solver.solve(n)));
+        bw.flush();
     }
+}
+
+class Solver {
+     public long solve(long n) {
+         return n;
+     }
 }
